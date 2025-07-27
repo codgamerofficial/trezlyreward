f72aead29aca5e8c79b3b653ebb77f85d5f7aecf
Saswata Dey <saswatadey700@gmail.com>
2025-07-27 19:03:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button>
    
    with the

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -99,7 +99,7 @@ export function AppLayout({children}: {children: ReactNode}) {
           <div className="flex items-center gap-4">
             <Button>
               <Wallet className="mr-2 h-4 w-4" />
-              Connect Wallet
+              Integrate Payment API
             </Button>
             <Link href="/profile">
               <Avatar>
